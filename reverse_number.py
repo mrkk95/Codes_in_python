f9a5e509008dca86007b0b752da159f7f2ebc2de
Keyur Khunadiya <53018341+mrkk95@users.noreply.github.com>
2020-10-02 18:42:09 +0530

    Update reverse_number.py

--- reverse_number.py
+++ reverse_number.py
@@ -1,8 +1,10 @@
 def Reverse(Number):  # Function Definition
-  rev = 0while(Number > 0):
+  rev = 0
+  while(Number > 0):
     rem = Number %10
     rev = (rev *10) + rem
-    Number = Number //10return rev
+    Number = Number //10
+  return rev
 
 Number = int(input())
 rev = Reverse(Number)  # Function Call
